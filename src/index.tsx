eae29255d0a0c2ba523ca10b4f5fc1d8f79b5237
Anton <92224243+tysh0nka@users.noreply.github.com>
2022-04-07 18:52:56 +0300

    Edit Photo

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,17 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import store from "./redux/store";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 
-
 ReactDOM.render(
-    <BrowserRouter>
-       <Provider store={store}>
-           <App/>
-       </Provider>
-    </BrowserRouter>,
+    <HashRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </HashRouter>,
     document.getElementById('root'));
-
-
 
 
 reportWebVitals();
